12a7b4396ed4805f99b40a8951509cbeb81ab22c
night-crawler <lilo.panic@gmail.com>
2026-08-29 16:16:28 +0100

    probably make leb128 a bit faster

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -10,6 +10,9 @@ pub mod typ;
 
 pub type IResult<I, O, E = Error> = Result<(I, O), E>;
 
+const DATA: u8 = 0x7F;
+const CONT: u8 = 0x80;
+
 fn parse_varuint<T>(input: &[u8]) -> IResult<&[u8], T>
 where
     T: TryFrom<u64>,
@@ -25,76 +28,51 @@ where
 }
 
 fn get_unsigned_leb128(input: &[u8]) -> Result<(u64, &[u8]), Error> {
-    const DATA: u8 = 0x7F;
-    const CONT: u8 = 0x80;
+    // We parse strings, realistically it will always be way less on average, so there's no point
+    // unfolding loops and messing with unneeded complexity.
+    let Some(head) = input.first_chunk::<3>() else {
+        return get_unsigned_leb128_slow(input);
+    };
 
+    let b0 = head[0];
+    if b0 & CONT == 0 {
+        return Ok((u64::from(b0), &input[1..]));
+    }
+    let value = u64::from(b0 & DATA);
+
+    let b1 = head[1];
+    if b1 & CONT == 0 {
+        return Ok((value | u64::from(b1) << 7, &input[2..]));
+    }
+    let value = value | u64::from(b1 & DATA) << 7;
+
+    let b2 = head[2];
+    if b2 & CONT == 0 {
+        return Ok((value | u64::from(b2) << 14, &input[3..]));
+    }
+
+    get_unsigned_leb128_slow(input)
+}
+
+#[cold]
+#[inline(never)]
+fn get_unsigned_leb128_slow(input: &[u8]) -> Result<(u64, &[u8]), Error> {
     let mut acc: u64 = 0;
 
-    if let Some(head) = input.first_chunk::<10>() {
-        macro_rules! read {
-            ($idx:expr, $shift:expr) => {{
-                let byte = head[$idx];
-                acc |= u64::from(byte & DATA) << $shift;
-                if byte & CONT == 0 {
-                    return Ok((acc, &input[$idx + 1..]));
-                }
-            }};
+    for (idx, &byte) in input.iter().take(10).enumerate() {
+        if byte & CONT == 0 {
+            if idx == 9 && byte > 1 {
+                return Err(Error::Overflow("varuint too large for u64".into()));
+            }
+            return Ok((acc | (u64::from(byte) << (idx * 7)), &input[idx + 1..]));
         }
-
-        read!(0, 0);
-        read!(1, 7);
-        read!(2, 14);
-        read!(3, 21);
-        read!(4, 28);
-        read!(5, 35);
-        read!(6, 42);
-        read!(7, 49);
-        read!(8, 56);
-
-        let b9 = head[9];
-        if b9 & CONT != 0 || b9 > 1 {
-            cold_path();
+        if idx == 9 {
             return Err(Error::Overflow("varuint too large for u64".into()));
         }
-
-        acc |= u64::from(b9) << 63;
-        return Ok((acc, &input[10..]));
+        acc |= u64::from(byte & DATA) << (idx * 7);
     }
 
-    // Slow path: fewer than 10 bytes left; check the length at every byte.
-    let len = input.len();
-    if len == 0 {
-        cold_path();
-        return Err(Error::Length(0));
-    }
-
-    macro_rules! read_checked {
-        ($idx:expr, $shift:expr) => {{
-            if len <= $idx {
-                cold_path();
-                return Err(Error::Length($idx));
-            }
-            let byte = input[$idx];
-            acc |= u64::from(byte & DATA) << $shift;
-            if byte & CONT == 0 {
-                return Ok((acc, &input[$idx + 1..]));
-            }
-        }};
-    }
-
-    read_checked!(0, 0);
-    read_checked!(1, 7);
-    read_checked!(2, 14);
-    read_checked!(3, 21);
-    read_checked!(4, 28);
-    read_checked!(5, 35);
-    read_checked!(6, 42);
-    read_checked!(7, 49);
-    read_checked!(8, 56);
-
-    // len <= 9 here: a continuation bit ran past the end of the buffer.
-    cold_path();
-    Err(Error::Length(9))
+    Err(Error::Length(input.len() + 1))
 }
 
 fn parse_u64<T>(input: &[u8]) -> IResult<&[u8], T>
