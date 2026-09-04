63bce7ae933da1a2b615b55e9be2ec038b3eb91a
night-crawler <lilo.panic@gmail.com>
2026-09-04 20:17:15 +0100

    slop: add perf helper scripts

--- benches/throughput.rs
+++ benches/throughput.rs
@@ -5,55 +5,14 @@ use std::{fs, hint::black_box, time::Duration};
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 mod common;
+#[path = "common/consume.rs"]
+mod consume;
 
-use chbr::FromBlock as _;
 use chbr::parse::block::parse_many;
 use criterion::{Criterion, Throughput, criterion_group, criterion_main};
 use testresult::TestResult;
 
-use crate::common::BenchmarkCols;
-
-fn consume_blocks(blocks: &[chbr::ParsedBlock<'_>]) -> TestResult<()> {
-    for row in BenchmarkCols::iter_blocks(blocks) {
-        let row = row?;
-
-        black_box(row.id);
-        black_box(row.lc_string_cd10);
-        black_box(row.timestamp);
-        black_box(row.count);
-        black_box(row.some_number);
-
-        black_box(row.lc_nullable_string_cd1000);
-        black_box(row.lc_nullable_string_cd5000);
-        black_box(row.lc_nullable_string_cd3000);
-        black_box(row.lc_nullable_string_cd4000);
-        black_box(row.lc_nullable_string_cd50000);
-        black_box(row.lc_nullable_string_cd100);
-        black_box(row.lc_nullable_string_cd500);
-        black_box(row.lc_nullable_string8);
-        black_box(row.lc_nullable_string_cd_00000);
-
-        black_box(row.some_ip_address);
-
-        for s in row.lc_tags {
-            black_box(s?);
-        }
-        for s in row.nested_lc_string_cd10 {
-            black_box(s?);
-        }
-        for &flag in row.nested_flag.try_as_slice()? {
-            black_box(flag);
-        }
-        for v in row.nested_some_id.try_as_slice()? {
-            black_box(v.get());
-        }
-        for v in row.nested_some_other_id.try_as_slice()? {
-            black_box(v.get());
-        }
-    }
-
-    Ok(())
-}
+use crate::consume::consume_blocks;
 
 fn consume_all(input: &[u8]) -> TestResult<()> {
     let blocks = parse_many(input)?;
